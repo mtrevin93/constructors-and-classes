cbb17e41585adb978e0e2472f2588cae1c4271a2
Michael Trevino <mtrevin93@gmail.com>
2021-08-25 12:48:45 -0400

    added company constructor, tested with first company

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace Classes
         static void Main(string[] args)
         {
             // Create an instance of a company. Name it whatever you like.
-
+            Company SeeSharper = new Company("SeeSharper", DateTime.Parse("5/1/2008"));
             // Create three employees
-
+Console.WriteLine(SeeSharper.CreatedOn);
             // Assign the employees to the company
 
             /*
