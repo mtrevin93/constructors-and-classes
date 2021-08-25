d92bd1c57da22c050537bb9864669b652de4392f
Michael Trevino <mtrevin93@gmail.com>
2021-08-25 12:55:13 -0400

    employee constructor & employee objects created

--- Employee.cs
+++ Employee.cs
@@ -10,6 +10,11 @@ namespace Classes
         public string LastName { get; }
         public string Title { get; }
         public DateTime StartDate { get; }
-
+        public Employee(string firstName, string lastName, string title, DateTime startDate){
+            FirstName = firstName;
+            LastName = lastName;
+            Title = title;
+            StartDate = startDate;
+        }
     };
 };
--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace Classes
             // Create an instance of a company. Name it whatever you like.
             Company SeeSharper = new Company("SeeSharper", DateTime.Parse("5/1/2008"));
             // Create three employees
-Console.WriteLine(SeeSharper.CreatedOn);
+            Employee JaneDoe = new Employee("Jane","Doe","HR Manager",DateTime.Parse("5/30/2008"));
+            Employee HowardSchultz = new Employee("Howard","Schultz","Barista",DateTime.Parse("1/1/2009"));
+            Employee SteveJobs = new Employee("Steve","Jobs","Lead Turtleneck-Wearer,",DateTime.Parse("3/1/2015"));
             // Assign the employees to the company
 
             /*
